3b2f62a84212d6112373885fc6204bd72f9c6057
Mayank Ashok <mayank@divertica.com>
2022-02-10 12:03:12 -0800

    update index.ts: create db connection, setup express server and add routes

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import { createConnection } from "typeorm";
+import express, { application } from "express";
+import { Banker } from "./entities/Banker";
+import { Client } from "./entities/Client";
+import { Transaction } from "./entities/Transactions";
+import { createClientRouter } from "./routes/create_client";
+import { createBankerRouter } from "./routes/create_banker";
+import { createTransactionRouter } from "./routes/create_transactions";
+
+const app = express();
+
+const main = async () => {
+  try {
+    await createConnection({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "mayank",
+      password: undefined,
+      database: "typeorm",
+      entities: [Client, Banker, Transaction],
+      synchronize: true,
+    });
+    console.log("connected to postgres");
+
+    app.use(express.json());
+    app.use(createClientRouter);
+    app.use(createBankerRouter);
+    app.use(createTransactionRouter);
+
+    app.listen(8080, () => {
+      console.log("listening on port 8080");
+    });
+  } catch (error) {
+    console.log(error);
+    throw new Error("Unable to connect to db");
+  }
+};
+
+main();
